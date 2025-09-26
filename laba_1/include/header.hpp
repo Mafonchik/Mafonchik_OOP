88a00332c512f0954dd3972dd6d1026e8d2d2c52
Mafonchik <nikoloztodua56816@gmail.com>
2025-09-26 19:52:39 +0300

    Create header.hpp

---
+++ laba_1/include/header.hpp
@@ -0,0 +1,10 @@
+#ifndef VALIDATION_REPLACE_H
+#define VALIDATION_REPLACE_H
+using namespace std;
+#include <string>
+
+bool validation(int n, char old_value, const string& text);
+
+string replace(string text, int n, char old_value, char new_value);
+
+#endif
